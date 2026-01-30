9fc87b86062bc0992fd6c9eff2541a32a944ed0b
LucasMonteir0 <lucasmonteirob1@outlook.com>
2026-01-30 16:17:07 -0300

    FIX: Widget incorrento chamado no runApp da aplicação

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import "package:flutter/material.dart";
 
+import "app.dart";
 import "modules/commons/config/dependency_injection.dart";
 import "modules/commons/utils/cache/app_cache.dart";
 
@@ -7,5 +8,5 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   DependencyInjection.init();
   await AppCache.instance.init();
-  runApp(const MyApp());
+  runApp(App());
 }

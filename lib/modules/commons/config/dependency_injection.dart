3c345bf7afb5b4684147331e4f840888ecafcc18
LucasMonteir0 <lucasmonteirob1@outlook.com>
2026-01-31 10:28:13 -0300

    FEAT: Instanciar dependencias do modulo AUTH

--- lib/modules/commons/config/dependency_injection.dart
+++ lib/modules/commons/config/dependency_injection.dart
@@ -1,6 +1,12 @@
 import "package:dio/dio.dart";
 import "package:get_it/get_it.dart";
 
+import "../../auth/core/data/data_sources/auth_data_source.dart";
+import "../../auth/core/data/data_sources/auth_data_source_impl.dart";
+import "../../auth/core/data/repositories/auth_repository_impl.dart";
+import "../../auth/core/domain/repositories/auth_repository.dart";
+import "../../auth/core/domain/use_cases/sign_in/sign_in_use_case.dart";
+import "../../auth/core/domain/use_cases/sign_in/sign_in_use_case_impl.dart";
 import "../core/data/services/http_service_impl.dart";
 import "../core/domain/services/http_service.dart";
 
@@ -9,6 +15,18 @@ final getIt = GetIt.instance;
 class DependencyInjection {
   DependencyInjection._();
   static void init() {
+    //SERVICES
     getIt.registerFactory<HttpService>(() => HttpServiceImpl(Dio()));
+
+    //DATA SOURCES
+    getIt.registerFactory<AuthDataSource>(() => AuthDataSourceImpl());
+
+    //REPOSITORIES
+    getIt.registerFactory<AuthRepository>(() => AuthRepositoryImpl());
+
+    //USE CASES
+    getIt.registerFactory<SignInUseCase>(() => SignInUseCaseImpl());
+
+    //BLOCS
   }
 }

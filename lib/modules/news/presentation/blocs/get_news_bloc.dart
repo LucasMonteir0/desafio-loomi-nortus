8ea61c501269fc7ab05f0730e4305121c2edbada
LucasMonteir0 <lucasmonteirob1@outlook.com>
2026-02-01 19:00:47 -0300

    FEAT: Criar gerenciamento de estado e instanciar dependencias do modulo NEWS

---
+++ lib/modules/news/presentation/blocs/get_news_bloc.dart
@@ -0,0 +1,63 @@
+import "package:flutter_bloc/flutter_bloc.dart";
+
+import "../../../commons/config/dependency_injection.dart";
+import "../../../commons/core/domain/entities/pagination.dart";
+import "../../core/domain/entities/news_item_entity.dart";
+import "../../core/domain/use_cases/get_news/get_news_use_case.dart";
+import "../states/pagination_state.dart";
+
+class GetNewsBloc extends Cubit<PaginationState<NewsItemEntity>> {
+  late final GetNewsUseCase _useCase;
+
+  List<NewsItemEntity> _items = [];
+  Pagination<NewsItemEntity>? _pagination;
+  int _currentPage = 0;
+
+  GetNewsBloc() : super(const PaginationInitialState()) {
+    _useCase = getIt<GetNewsUseCase>();
+  }
+
+  bool get _hasMore => _pagination?.hasNextPage ?? true;
+
+  void load({bool refresh = false}) async {
+    if (refresh) {
+      _items = [];
+      _currentPage = 0;
+      _pagination = null;
+    }
+
+    if (state.isLoading) {
+      return;
+    }
+    if (!_hasMore && !refresh) {
+      return;
+    }
+
+    emit(
+      PaginationLoadingState(
+        currentItems: _items,
+        isLoadingMore: _items.isNotEmpty,
+      ),
+    );
+
+    final result = await _useCase.call(_currentPage + 1);
+
+    if (result.isSuccess) {
+      _currentPage = result.data!.page;
+      _pagination = result.data!;
+      _items = [..._items, ...result.data!.data];
+      emit(
+        PaginationSuccessState(
+          items: _items,
+          pagination: _pagination!,
+          hasMore: _pagination!.hasNextPage,
+        ),
+      );
+      return;
+    }
+
+    emit(PaginationErrorState(error: result.error, currentItems: _items));
+  }
+
+  void refresh() => load(refresh: true);
+}
---
+++ lib/modules/news/presentation/states/pagination_state.dart
@@ -0,0 +1,62 @@
+import "package:equatable/equatable.dart";
+
+import "../../../commons/core/domain/entities/base_error.dart";
+import "../../../commons/core/domain/entities/pagination.dart";
+import "../../../commons/utils/errors/errors.dart";
+
+sealed class PaginationState<T> extends Equatable {
+  const PaginationState();
+}
+
+final class PaginationInitialState<T> extends PaginationState<T> {
+  const PaginationInitialState();
+
+  @override
+  List<Object?> get props => [];
+}
+
+final class PaginationLoadingState<T> extends PaginationState<T> {
+  final List<T> currentItems;
+  final bool isLoadingMore;
+
+  const PaginationLoadingState({
+    this.currentItems = const [],
+    this.isLoadingMore = false,
+  });
+
+  @override
+  List<Object?> get props => [currentItems, isLoadingMore];
+}
+
+final class PaginationSuccessState<T> extends PaginationState<T> {
+  final List<T> items;
+  final Pagination<T> pagination;
+  final bool hasMore;
+
+  const PaginationSuccessState({
+    required this.items,
+    required this.pagination,
+    required this.hasMore,
+  });
+
+  @override
+  List<Object?> get props => [items, pagination, hasMore];
+}
+
+final class PaginationErrorState<T> extends PaginationState<T> {
+  final BaseError error;
+  final List<T> currentItems;
+
+  PaginationErrorState({BaseError? error, this.currentItems = const []})
+    : error = error ?? UnknownError();
+
+  @override
+  List<Object?> get props => [error, currentItems];
+}
+
+extension PaginationStateExtension<T> on PaginationState<T> {
+  bool get isInitial => this is PaginationInitialState<T>;
+  bool get isLoading => this is PaginationLoadingState<T>;
+  bool get isSuccess => this is PaginationSuccessState<T>;
+  bool get isError => this is PaginationErrorState<T>;
+}

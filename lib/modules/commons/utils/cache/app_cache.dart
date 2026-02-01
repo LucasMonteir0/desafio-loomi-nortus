ed9da21938c2478a1ae61884d0f6fd46698b3ef4
LucasMonteir0 <lucasmonteirob1@outlook.com>
2026-02-01 15:15:56 -0300

    FEAT: Crie a Autenticação de Usuároo

--- lib/modules/commons/utils/cache/app_cache.dart
+++ lib/modules/commons/utils/cache/app_cache.dart
@@ -7,8 +7,11 @@ class AppCache {
 
   late final SharedPreferences _prefs;
 
+  late final bool _isLogged;
+
   Future<void> init() async {
     _prefs = await SharedPreferences.getInstance();
+    _isLogged = _prefs.getBool("rememberUser") ?? false;
   }
 
   void setRememberUser(bool value) {
@@ -17,5 +20,13 @@ class AppCache {
 
   bool getRememberUser() {
     return _prefs.getBool("rememberUser") ?? false;
+  }
+
+  bool get isLogged {
+    return _isLogged;
+  }
+
+  void setIsLogged(bool value) {
+    _isLogged = value;
   }
 }
